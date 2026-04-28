4d807fcd27756d677aeed8305c860f444b0fc649
kevingents <kevin@gents.nl>
2026-04-28 13:20:08 +0200

    Create return-refund.js

---
+++ api/return-refund.js
@@ -0,0 +1,12 @@
+{
+  "orderId": "123456789",
+  "employeeName": "Naam medewerker",
+  "reason": "Maat te klein",
+  "confirm": true,
+  "items": [
+    {
+      "lineItemId": "987654321",
+      "quantity": 1
+    }
+  ]
+}
